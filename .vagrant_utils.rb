a9decc043f9973644db9ff3339970585500c26f9
Luca Invernizzi <Invernizzi.l@gmail.com>
2015-01-20 15:30:48 -0800

    more options

--- .vagrant_utils.rb
+++ .vagrant_utils.rb
@@ -11,11 +11,11 @@ def provision_with_puppet(machine, manifest)
   machine.vm.provision :shell, path: ".run_puppet_manifest.sh", args: manifest, keep_color: true
 end
 
-def run_on_host(machine, host)
+def run_on_host(machine, options)
   machine.vm.provider :libvirt do |libvirt|
-    libvirt.host = host
-    libvirt.memory = 2048
-    libvirt.cpus = 4
+    libvirt.host = options[:host]
+    libvirt.memory = options[:memory] || 2048
+    libvirt.cpus = options[:cpus] || 4
   end
 end
 
@@ -35,7 +35,7 @@ end
 def define_a_vm(config, options)
   config.vm.define options[:name] do |machine|
     set_external_ip(options[:ip]) if options[:ip]
-    run_on_host(machine, options[:host])
+    run_on_host(machine, options)
     provision_with_puppet(machine, options[:manifest])
   end
 end
--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,11 @@ require_relative '.vagrant_utils.rb'
 
 # Hosts -----------------------------------------------------------------------
 as_my_virtual_machines do |config|
+
+  # The simplest VM
   define_a_vm config, name: "test", host: 'veni', manifest: 'test.pp'
+
+  # A more customized VM
+  define_a_vm config, name: "big_mama", host: 'veni', manifest: 'test.pp', memory: 4096, cpus: 10, ip: '192.168.48.196'
 end
 # -----------------------------------------------------------------------------
