2dae34e883838c463ca5107d6ce78ec48148c85e
Luca Invernizzi <Invernizzi.l@gmail.com>
2015-01-26 13:10:00 -0800

    easier to change provisioning backend

--- .vagrant_utils.rb
+++ .vagrant_utils.rb
@@ -1,6 +1,5 @@
 # Backend config --------------------------------------------------------------
 #
-ENV['VAGRANT_DEFAULT_PROVIDER'] = 'libvirt'
 # -----------------------------------------------------------------------------
 
 
@@ -20,15 +19,20 @@ def run_on_host(machine, options)
 end
 
 def set_external_ip(machine, ip)
-  machine.vm.network :public_network, ip: ip, dev: "br0", mode: 'bridge'
+  machine.vm.network :public_network, ip: ip, dev: "br192", mode: 'bridge'
 end
 
-def set_defaults(config)
+def set_defaults(config, provider)
   config.vm.box = "trusty64"
-  config.vm.provider :libvirt do |libvirt|
-    libvirt.username = 'root'
-    libvirt.connect_via_ssh = true
-    libvirt.default_prefix = nil
+  case provider.to_sym
+  when :virtualbox
+  when :libvirt
+    ENV['VAGRANT_DEFAULT_PROVIDER'] = 'libvirt'
+    config.vm.provider :libvirt do |libvirt|
+      libvirt.username = 'root'
+      libvirt.connect_via_ssh = true
+      libvirt.default_prefix = nil
+    end
   end
 end
 
@@ -36,14 +40,14 @@ def define_a_vm(config, options)
   config.vm.define options[:name] do |machine|
     set_external_ip(machine, options[:ip]) if options[:ip]
     machine.vm.host_name = options[:name]
-    run_on_host(machine, options)
+    run_on_host(machine, options) if options[:host]
     provision_with_puppet(machine, options[:manifest]) if options[:manifest]
   end
 end
 
-def as_my_virtual_machines
+def provisioning_with(provider)
   Vagrant.configure("2") do |config|
-    set_defaults(config)
+    set_defaults(config, provider)
     yield config
   end
 end
--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 require_relative '.vagrant_utils.rb'
 
 # Hosts -----------------------------------------------------------------------
-as_my_virtual_machines do |config|
+provisioning_with('libvirt') do |config|
 
   # The simplest VM
   define_a_vm config, name: "test", host: 'veni', manifest: 'test.pp'
